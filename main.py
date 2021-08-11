b1e18263bcc849bc42fa3b79bd977686413f7de1
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-08-11 14:28:24 +0000

    human guess complete

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ import random;
 # creating a function for the computer guess
 def guess(x):
     randomNumber = random.randint(1, x);
-    i = 0;
-    for i in randomNumber:
-        i = input(f"Guess a number between 1 and {x}")
-        
+    guessNo = 0;
+    while (guessNo != randomNumber):
+        guessNo = int(input(f"enter a number between 1 and {x}: "));
+        if guessNo > randomNumber: 
+            print("number too large try again")
+        elif guessNo < randomNumber: 
+            print("number too small try again")
+    print(f"Yaaaayyyyy {x} is the correct number. thanks for playing")
+
+guess(100)
+
+
 
 
