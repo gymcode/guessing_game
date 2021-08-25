c953d6000170af785d90bf808d0a31fc591bfd81
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-08-25 16:32:19 +0000

    rock paper scissors

--- r_p_s.py
+++ r_p_s.py
@@ -1,8 +1,29 @@
+
+
 import random; 
 
-human = input(f"what is your choice? Rock(r), Paper(p), Scissors(s): ");
-computer = random.choice("r", "p", "s");
 
-
-def is_win(person, computer):
+def play():
     
+    person = input(f"what is your choice? Rock(r), Paper(p), Scissors(s): ");
+    computer = random.choice(["r", "p", "s"]);
+    print(f"I the computer's choice is {computer}")
+
+    if (person == computer): 
+        return "It is a tie"
+
+    if is_win(person, computer): 
+        return "You won"
+
+    return "You Lost"
+
+
+
+#  function for winning 
+def is_win(person, computer):
+    if (person == "r" and computer == "s") or (person == "s" and computer == "p") or (person == "p" and computer == "r"): 
+        return True;  
+
+
+
+print(play())
